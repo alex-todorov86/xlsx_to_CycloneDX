c587dee035eb07d0bf3f8956db8695e1037c07da
Aleksandar Todoro <linked2u@gmail.com>
2020-12-08 00:40:01 +0200

    added funct to convert args to kwargs

--- main_prog.py
+++ main_prog.py
@@ -3,4 +3,21 @@ import pandas as pd
 import json
 
 
+##################################
+#        FUNCTIONS BLOCK         #
+##################################
 
+
+def args_to_kwargs(args_list):
+    '''
+    Converts args to kwargs.
+    An arg is considered as kwarg
+    when it's preceeded with a dash (-)
+    '''
+    out = {}
+    for i in range(1, len(args_list)):
+        if args_list[i].startswith('-'):
+            out[args_list[i]] = args_list[i+1]
+    return out
+
+#print(args_to_kwargs(argv))
