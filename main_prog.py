f21cd43d3d44b2c0485abe4f5b0f9fee34a9472e
Aleksandar Todoro <linked2u@gmail.com>
2020-12-08 02:59:54 +0200

    successfully tested write_out method

--- main_prog.py
+++ main_prog.py
@@ -36,14 +36,14 @@ def args_to_kwargs(args_list):
 
 class CycloneDX_BOM:
 
-    def __init__(self, out_file='test.out'):
+    def __init__(self, out_file='test.json'):
         self.body = {
-                   'bomFormat': 'CycloneDX',
-                   'specVersion': 1.2,
-                   'serialNumber': 'urn:uuid:{0}'.format(uuid.uuid4()),
-                   'version': 1,
-                   'metadata': 'This is a test BOM',
-                   'components': [
+                   "bomFormat": "CycloneDX",
+                   "specVersion": 1.2,
+                   "serialNumber": "urn:uuid:{0}".format(uuid.uuid4()),
+                   "version": 1,
+                   "metadata": "This is a test BOM",
+                   "components": [
 
                        ]
                     }
@@ -52,20 +52,15 @@ class CycloneDX_BOM:
     def add_component(self, publisher, name,
                       version, ctype):
 
-        component = {'type': ctype, 'publisher': publisher,
-                     'name': name, 'version': version}
+        component = {"type": ctype, "publisher": publisher,
+                     "name": name, "version": version}
 
         self.body['components'].append(component)
 
-   def write_out(self, out_file):
-       json.loads(self.body)
-
-
-
-
-
-
-
+    def write_out(self):
+        data = json.dumps(self.body)
+        with open(self.out_file, 'w') as output:
+            output.write(data)
 
 ##################################
 #          MAIN CODE             #
@@ -77,3 +72,4 @@ test.add_component('myCorp', 'test2', 1.0, 'rocket')
 test.add_component('whyCorp', 'test1', 1.0, 'rocket')
 print(test.body)
 print(kwargs)
+test.write_out()
